d7bda5748e7a2d1189979f94a8a0fb5bbe42b1e8
o.hrachov <ol.grachov@gmail.com>
2023-11-17 18:29:48 +0200

    feat: add basic calendar layout

---
+++ src/components/CalendarHeader/CalendarHeader.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './CalendarHeader.scss';
+
+export default function CalendarHeader() {
+  return (
+    <div className='calendar__header'>CalendarHeader</div>
+  )
+}
